0e45f9c88d2cdb9a89d024886d330cc40e81dc2a
Robert Tate <btate712@gmail.com>
2019-03-10 16:39:37 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -63,7 +63,7 @@ def won?
   WIN_COMBINATIONS.each do |combo|
     if (@board[combo[0]] == "X" || @board[combo[0]] == "O") &&
       (@board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]])
-      return true
+      return combo
     end
   end
   return false
